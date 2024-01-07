afa980ee408e2af53fe90422eefedbdb7e385989
geekelo <efueliht12@gmail.com>
2024-01-07 18:56:01 +0100

    🟢 Fix bugs

--- src/components/certificate.js
+++ src/components/certificate.js
@@ -36,8 +36,8 @@ function Certificate({ foundCertificate }) {
       useCORS: true,
       scrollX: 0,
       scrollY: 0,
-      width: 780,
-      height: 1208,
+      width: 1480,
+      height: 2508,
     });
 
     const imgData = canvas.toDataURL('image/png');
