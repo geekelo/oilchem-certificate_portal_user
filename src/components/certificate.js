8fbad63d1f518c2df1594bfea45007372fcdfbc4
geekelo <efueliht12@gmail.com>
2024-01-07 19:15:53 +0100

    🟢 Fix bugs

--- src/components/certificate.js
+++ src/components/certificate.js
@@ -36,7 +36,7 @@ function Certificate({ foundCertificate }) {
       useCORS: true,
       scrollX: 0,
       scrollY: 0,
-      width: '80%',  // Example using percentage
+      width: '80%', // Example using percentage
       height: '80vh', // Example using viewport height
     });
 
@@ -45,7 +45,7 @@ function Certificate({ foundCertificate }) {
     const pdf = new jsPDF({
       orientation: 'landscape',
       unit: 'mm',
-      format: [297, 210], 
+      format: [297, 210],
     });
     pdf.addImage(imgData, 'PNG', 0, 0);
     pdf.save('certificate.pdf');
